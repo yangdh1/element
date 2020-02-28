0b8ca3260959c7ddd2b9cff2fcec8e9ef96835cc
sumeihuachenyu <18209571211@163.com>
2020-02-28 22:08:16 +0800

    提交用户管理

--- src/views/user/userList.vue
+++ src/views/user/userList.vue
@@ -5,6 +5,43 @@
     </div>
     <div>
       <el-form :inline="true" :model="pars" class="demo-form-inline">
+         <el-form-item label="">
+         <el-select v-model="pars.whetherLawyer"
+                    placeholder="请选择用户类型"
+                    size="small"
+                    clearable
+                    style="width: 120px">
+           <el-option v-for="item in whetherLawyerList"
+                      :key="item.id"
+                      :label="item.name"
+                      :value="item.whetherLawyer">
+           </el-option>
+         </el-select>
+         </el-form-item>
+           <el-form-item label="认证状态">
+           <el-select v-model="pars.whetherAutonym"
+                      placeholder="请选择认证状态"
+                      size="small"
+                      clearable
+                      style="width: 120px">
+             <el-option v-for="item in whetherAutonymList"
+                        :key="item.id"
+                        :label="item.name"
+                        :value="item.whetherAutonym">
+             </el-option>
+           </el-select>
+       </el-form-item>
+        <el-form-item label="注册日期：">
+          <div class="block">
+            <el-date-picker
+              v-model="pars.timeString"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期">
+            </el-date-picker>
+          </div>
+        </el-form-item>
         <el-form-item label="">
           <el-input v-model="pars.commonColumn"   clearable size="small" style="width: 350px" prefix-icon="el-icon-search" placeholder="输入搜索关键字，多个关键字用空格分开"></el-input>
         </el-form-item>
@@ -32,22 +69,61 @@
         <el-table highlight-current-row :data="tableData" width="100%" border stripe :header-cell-style="headercellSstyle" :key="$root.$children[0].keyRand">
           <el-table-column
             prop="nickName"
+            fixed
             label="昵称"
             min-width="15%"/>
           <el-table-column
             prop="name"
+            fixed
             label="真实姓名"
             min-width="15%"/>
           <el-table-column
             prop="mobile"
             label="手机号"
+            fixed
+            min-width="15%"/>
+<!--          <el-table-column-->
+<!--          prop="roleName"-->
+<!--          label="角色"-->
+<!--          min-width="15%"/>-->
+          <el-table-column
+            prop="moneyBalance"
+            label="账户余额"
             min-width="15%"/>
           <el-table-column
-            prop="roleName"
-            label="角色"
+            prop="lawyerEarningsBalance"
+            label="账户收益"
             min-width="15%"/>
           <el-table-column
-            label="操作" min-width="40%">
+            prop="coinBalance"
+            label="心币"
+            min-width="15%"/>
+          <el-table-column
+            prop="followNum"
+            label="关注人数"
+            min-width="15%"/>
+          <el-table-column
+            prop="serviceNum"
+            label="服务次数"
+            min-width="15%"/>
+          <el-table-column
+            prop="createTimeDate"
+            label="注册日期"
+            min-width="15%">
+          </el-table-column>
+          <el-table-column
+            prop="whetherAutonym"
+            fixed="right"
+            label="认证状态"
+            min-width="15%">
+            <template slot-scope="scope">
+              <span v-if="scope.row.whetherAutonym === 1">未实名认证</span>
+              <span v-else>已实名认证</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作" min-width="30%">
             <template slot-scope="scope">
               <el-button
                 icon="el-icon-view"
@@ -83,14 +159,47 @@
       return {
         tableData:[],
         roleArr:[],
+        whetherLawyerList:[
+          {
+            name: '全部',
+            whetherAutonym: 0
+          },
+          {
+            name: '普通用户',
+            whetherLawyer: 1
+          },
+          {
+            name: '律师',
+            whetherLawyer: 2
+          },
+          {
+            name: '企业',
+            whetherLawyer: 3
+          }
+        ],
+        whetherAutonymList:[{
+          name: '全部',
+          whetherAutonym: 0
+        },{
+          name: '未实名认证',
+          whetherAutonym: 1
+        },{
+          name: '已实名认证',
+          whetherAutonym: 2
+        }],
         loading  : true,
         pars: {
           pageSize    : this.GLOBAL.PAGE_COG.PAGESIZE,
           pageNum     : this.GLOBAL.PAGE_COG.PAGENUM,
           total       : this.GLOBAL.PAGE_COG.TOTAL,
-          userName  : '',
-          roleId : '',
-        }
+          commonColumn: '',
+          whetherLawyer: 1,
+          whetherAutonym: 0,
+          timeString: '',
+          startDate: '',
+          endTime: ''
+        },
+
       }
     },
     mounted(){
@@ -118,6 +227,17 @@
       },
       search(){
         this.pars.pageNum=1;
+        if(this.pars.timeString != null){
+          if(this.pars.timeString[0] != null){
+            this.pars.startDate = this.pars.timeString[0];
+          }
+          if(this.pars.timeString[1] != null){
+            this.pars.endTime = this.pars.timeString[1];
+          }
+        }else{
+          this.pars.startDate = '';
+          this.pars.endTime = '';
+        }
         this.loadData();
       },
       addDev(){
