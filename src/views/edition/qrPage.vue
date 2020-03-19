14505db038a6459d4915e014412a503c034e54ab
ydh <923760795@qq.com>
2020-03-19 17:07:30 +0800

    服务类型修改

--- src/views/edition/qrPage.vue
+++ src/views/edition/qrPage.vue
@@ -1,9 +1,9 @@
 <template>
     <el-tabs v-model="activeName" type="card">
-      <el-tab-pane label="普通用户端" name="second">
+      <el-tab-pane label="app" name="second">
         <div style="padding-top: 15%;padding-left:35%;">
           <div style="width: 300px;height: 400px;background: #86DB48">
-            <div style="font-size: 20px;padding-top: 30px;padding-left: 40px;color: white">普通用户端APP</div>
+            <div style="font-size: 20px;padding-top: 30px;padding-left: 40px;color: white">APP</div>
             <!-- <vue-qr  :logoSrc="config.logo" :text="config.value" :size="200" :margin="0"></vue-qr> -->
             <vue-qr style="padding-left: 50px;padding-top: 30px" :text="config.propertyUrl" :size="200" :margin="0"></vue-qr>
             <div style="font-size: 20px;padding-left: 60px;padding-top: 20px;color: white">扫一扫下载最新版本</div>
@@ -20,16 +20,16 @@
           </div>
         </div>
       </el-tab-pane>-->
-      <el-tab-pane label="律师用户端" name="first">
+<!--      <el-tab-pane label="律师用户端" name="first">
         <div style="padding-top: 15%;padding-left:35%;">
           <div style="width: 300px;height: 400px;background: #86DB48">
             <div style="font-size: 20px;padding-top: 30px;padding-left: 40px;color: white">律师用户端APP</div>
-            <!-- <vue-qr  :logoSrc="config.logo" :text="config.value" :size="200" :margin="0"></vue-qr> -->
+            &lt;!&ndash; <vue-qr  :logoSrc="config.logo" :text="config.value" :size="200" :margin="0"></vue-qr> &ndash;&gt;
             <vue-qr style="padding-left: 50px;padding-top: 30px" :text="config.logisticsUrl" :size="200" :margin="0"></vue-qr>
             <div style="font-size: 20px;padding-left: 60px;padding-top: 20px;color: white">扫一扫下载最新版本</div>
           </div>
         </div>
-      </el-tab-pane>
+      </el-tab-pane>-->
     </el-tabs>
 </template>
 
