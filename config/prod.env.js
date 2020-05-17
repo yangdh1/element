18dc41171b7ce203a3d8d71cea46b5ff9e55b065
ydh <18558244350@163.com>
2020-05-17 16:22:48 +0800

    ti

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  //测试
-  BASE_API: '"http://114.116.7.95:9201/ms-platform"',
-  MULTIPART_API: '"http://114.116.7.95:9202/ms-multipart"',
+  BASE_API: '"http://localhost:8201/microsands-platform"',
 
-  //线上
-  //BASE_API: '"http://114.116.7.95:8201/ms-platform"',
-  //MULTIPART_API: '"http://114.116.7.95:8202/ms-multipart"',
+  MULTIPART_API: '"http://localhost:8201/microsands-platform"',
 }
